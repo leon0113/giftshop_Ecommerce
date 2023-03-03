4480cf973b523981f0ca9bbaf5c7ff218be22155
leon0113 <tahjib.leon399@gmail.com>
2023-03-04 02:22:28 +0600

    npm packages installed

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-const express = require('express')
-const colors = require('colors')
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+
+//configure env
+dotenv.config();
+
 //rest object
 const app = express()
 
 //rest api
 
 app.get('/', (req, res) => {
-    res.send({
-        message: 'Welcome to ecommerce app'
-    })
+    res.send("<h1>Welcome to ecommerce app</h1>")
 })
 
 //port 
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080;
 
 // run  listen
 app.listen(PORT, () => {
-    console.log(`server running on ${PORT}`.bgCyan.white);
+    console.log(`server running on ${process.env.DEV_MODE} mode on ${PORT}`.bgCyan.white);
 })
